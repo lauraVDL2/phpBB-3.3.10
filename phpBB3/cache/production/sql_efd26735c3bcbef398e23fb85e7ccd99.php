1cbb1e625717abd23e05c6a608e613eafc36c5f2
lauraVDL2 <laura.vanderlinden@hotmail.com>
2023-06-25 13:07:18 +0200

    guidebook v1

--- phpBB3/cache/production/sql_efd26735c3bcbef398e23fb85e7ccd99.php
+++ phpBB3/cache/production/sql_efd26735c3bcbef398e23fb85e7ccd99.php
@@ -1,5 +1,5 @@
 <?php exit; ?>
-1687623203
+1687691591
 SELECT forum_id FROM phpbb_forums WHERE forum_options & 2 <> 0 LIMIT 1
 6
 a:0:{}
---
+++ phpBB3/guidebook.php
@@ -0,0 +1,25 @@
+<?php 
+define('IN_PHPBB', true);
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+include($phpbb_root_path . 'common.' . $phpEx);
+include($phpbb_root_path . 'includes/functions_display.' . $phpEx);
+
+// Start session management
+$user->session_begin();
+$auth->acl($user->data);
+$user->setup('viewforum');
+
+$page_title = ($config['board_index_text'] !== '') ? $config['board_index_text'] : $user->lang['INDEX'];
+
+$vars = array('page_title');
+extract($phpbb_dispatcher->trigger_event('core.index_modify_page_title', compact($vars)));
+
+// Output page
+page_header($page_title, true);
+
+$template->set_filenames(array(
+	'body' => 'guidebook.html')
+);
+
+page_footer();
